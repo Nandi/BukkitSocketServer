859df5897bccd21c5b13e7045ca45791ec6deaf5
Simon Bakkevig <simon.nandi@gmail.com>
2011-06-12 10:42:35 +0200

    Emptied all the functions until HTTPServer is properly set up

--- src/me/hedgehog/bukkitsocketserver/CommandHandler.java
+++ src/me/hedgehog/bukkitsocketserver/CommandHandler.java
@@ -10,14 +10,7 @@ import java.util.List;
 
 import net.minecraft.server.MinecraftServer;
 
-import org.bukkit.World;
 import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.plugin.Plugin;
-
-import org.jdom.*;
 
 public class CommandHandler {
 	
@@ -29,7 +22,7 @@ public class CommandHandler {
 	}
 	
 	// Kicks a list of players
-	public void kickPlayer(List<String> args){
+	public void kickPlayer(String[] args){
 		for(String arg : args){
 			if(plugin.getServer().getPlayer(arg) != null){
 				plugin.getServer().getPlayer(arg).kickPlayer("You have been kicked by the webadmin.");
@@ -38,7 +31,7 @@ public class CommandHandler {
 	}
 	
 	// Bans a list of players
-	public void banPlayer(List<String> args){
+	public void banPlayer(String[] args){
 		for(String arg : args){
 			if(plugin.getServer().getPlayer(arg) != null){
 				consoleCommand("ban " + arg);
@@ -47,134 +40,23 @@ public class CommandHandler {
 	}
 	
 	// Gets plugin list
-	public Element pluginList(){
-		Element rootElement = new Element("pugins");
-		
-		Plugin[] pluginList = plugin.getServer().getPluginManager().getPlugins();
-		
-		for(Plugin p : pluginList){
-			Element plug = new Element("plugin");
-			plug.setAttribute("name",p.getDescription().getName());
-			plug.setAttribute("version", p.getDescription().getVersion());
-		}
-		
-		return rootElement;
+	public void pluginList(){
 	}
 	
 	// Gets a list of all players, takes world name as arguments
-	public Element playerList(List<String> args){
-		Element rootElement = new Element("worlds");
-		
-		List<World> worlds = new LinkedList<World>();
-		if(args.size() != 0){
-			for(int i = 0; i < args.size(); i++){
-				worlds.add(plugin.getServer().getWorld(args.get(i)));
-	    	}
-			
-		}
-		else{
-    		worlds = plugin.getServer().getWorlds();
-		}
-		
-		for(int i=0;i<worlds.size();i++){
-			Element world = new Element("world");
-			rootElement.addContent(world);
-			world.setAttribute("name", worlds.get(i).getName());
-			
-			Element players = new Element("players");
-			world.addContent(players);
-			
-			List<Player> playerList = worlds.get(i).getPlayers();
-			if(playerList.size() == 0){
-				players.setAttribute("empty", "true");
-			}
-			else{
-				players.setAttribute("empty", "false");
-			}
-			for(int j=0;j<playerList.size();j++){
-				Element player = new Element("player");
-				player.setAttribute("name", playerList.get(j).getName());
-				player.setAttribute("dispName", playerList.get(j).getDisplayName());
-				player.setAttribute("dead", Boolean.toString(playerList.get(j).isDead()));
-				players.addContent(player);
-			}
-		}
-		
-		return rootElement;
+	public void playerList(String[] args){
 	}
 	
 	// Gets players inventory takes player name as argument
-	public Element playerInventory(List<String> args){
-		Element rootElement = new Element("players");
-		
-		List<Player> players = new LinkedList<Player>();
-		for(String arg : args){
-			if (plugin.getServer().getPlayer(arg) == null)
-				return errorString("One or all of the players listed is not online");
-			else
-				players.add(plugin.getServer().getPlayer(arg));
-		}
-		
-		
-		for(Player p : players){
-			//format xml with inventory
-			Element player = new Element("player");
-			player.setAttribute("name", p.getName());
-			rootElement.addContent(player);
-			
-			PlayerInventory inv = p.getInventory();
-			ItemStack[] invCont = inv.getContents();
-			
-			Element inventory = new Element("inventory");
-			player.addContent(inventory);
-			
-			for(int i = 0; i< invCont.length; i++){
-				Element item = new Element("item");
-				
-				
-				
-				//Check if empty slot!
-				if (invCont[i] == null){
-					item.setAttribute("empty", Boolean.toString(true));
-				}
-				else {
-					item.setAttribute("empty", Boolean.toString(false));
-					item.setAttribute("id", Integer.toString(invCont[i].getTypeId()));
-					item.setAttribute("name", invCont[i].getType().name());
-					item.setAttribute("stackSize", Integer.toString(invCont[i].getAmount()));
-				}
-				inventory.addContent(item);
-			}
-		}
-
-		return rootElement;
+	public void playerInventory(String[] args){
 	}
 	
 	// Gets the console output since last start
-	public Element getConsole(){
-		List<String> lines = readConsole();
-		
-		Element rootElement = new Element("console");
-		
-		int start = 0;
-		for(int i=0;i<lines.size();i++){
-			if(lines.get(i).indexOf("[INFO] Starting minecraft server") != -1)
-				start = i;
-		}
-		for(int i=start;i<lines.size();i++){
-			Element line = new Element("line");
-			line.setText(lines.get(i));
-			rootElement.addContent(line);
-		}
-		return rootElement;
+	public void getConsole(){
 	}
 
 	// Gets a standard responce to a unrecognized command
-	public Element errorString(String err){
-		Element rootElement = new Element("error");
-		rootElement.setText(err);
-		
-		return rootElement;
+	public void errorString(String err){
 	}
 	
 	// Read the server log
@@ -195,6 +77,7 @@ public class CommandHandler {
 	}
 	
 	// Send commands to the console
+	
 	private void consoleCommand(String cmd){
 		CraftServer craftserver = (CraftServer)plugin.getServer();
 		Field field;
