43719fc76ce6460cb5b82d6512cf47e498652320
Simon Bakkevig <simon.nandi@gmail.com>
2011-06-13 13:18:01 +0200

    Changed to remoteAddr from InetSocketAddress to InetAddress

--- src/me/hedgehog/bukkitsocketserver/tools/HttpRequest.java
+++ src/me/hedgehog/bukkitsocketserver/tools/HttpRequest.java
@@ -1,14 +1,16 @@
 package me.hedgehog.bukkitsocketserver.tools;
 
 import java.io.InputStream;
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
+	public InetAddress remoteAddr;
     public String method;
     public String path;
     public String query;
     public String version;
     public Map<String, String> fields = new HashMap<String, String>();
-    public InputStream body;    
+    public InputStream body;
 }
